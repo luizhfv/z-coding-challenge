f7d24aa0d9885b3761585d491616f661621e1f5f
flaviuse <flavien.rebattet@grenoble-inp.org>
2024-05-27 12:01:42 +0200

    Fix testing setup

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,7 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/tests/__mocks__/cssMock.js',
+    '\\.(css|less)$': '<rootDir>/cssMock.js',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
 }
